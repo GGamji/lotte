9e6d284b1fd40dc363072d8fa375a60d70501689
GGamji <wndusrj98@naver.com>
2024-10-29 18:09:20 +0900

    sec commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
+$(window).scroll(function() {
+	if($(window).scrollTop() >= 10) {
+		$('header').addClass('scroll')
+	}
+	else {
+		$('header').removeClass('scroll')
+	}
+})
+
+
 
 // Header
 $('#gnb .dep1').hover(function() {
 	$('#gnb .dep2_wrap').stop().slideDown()
 	$('header').addClass('on')
-	// $('.dep2_wrpa').addClass('bg')
+	// $('.dep2_wrap').addClass('bg')
 }, function() {
 	$('#gnb .dep2_wrap').stop().slideUp();
 	$('header').removeClass('on')
-	// $('.dep2_wrpa').removeClass('bg')
+	// $('.dep2_wrap').removeClass('bg')
 });
 // $('#gnb .dep1').mouseleave()
 
@@ -48,3 +58,11 @@ const promotionSwiper = new Swiper('.promotion_swiper', {
 	slidesPerView: 4,
 	spaceBetween: 30,
 })  //이거땜에 카드가 늘어남 고민을 해야 됨
+
+
+
+
+// Footer
+$('footer .footer_familysite').click(function() {
+	$('footer .familysite_content').toggle()
+})
